6047255449cf405477de0f8d86139a150c8a91af
Gokcehan <gokcehankara@gmail.com>
2017-02-11 16:45:23 +0300

    Revert "read response in remote commands"
    This reverts commit 46a43785319bf0453441050b0b461ab45e7cd443.

--- client.go
+++ client.go
@@ -166,15 +166,5 @@ func sendRemote(cmd string) error {
 
 	fmt.Fprintln(c, cmd)
 
-	s := bufio.NewScanner(c)
-
-	s.Scan()
-
-	fmt.Println(s.Text())
-
-	if s.Err() != nil {
-		return fmt.Errorf("reading response: %s", s.Err())
-	}
-
 	return nil
 }
