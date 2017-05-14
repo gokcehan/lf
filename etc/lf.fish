bf03f061ee27d665df28fca7263fe51ca6ee0441
Gokcehan <gokcehankara@gmail.com>
2017-05-14 17:11:38 +0300

    cleanup

--- etc/lf.fish
+++ etc/lf.fish
@@ -1,6 +1,12 @@
-# Change working dir in Fish to last dir in lf on exit (adapted from Bash version)
+# Change working dir in Fish to last dir in lf on exit (adapted from ranger).
 #
-# You need to copy the content of this file to ~/.config/fish/config.fish
+# You need to either copy the content of this file to ~/.config/fish/config.fish
+# or source this file directly using something like:
+#
+#     set lffish "$GOPATH/src/github.com/gokcehan/lf/etc/lf.fish"
+#     if test -f "$lffish"
+#         source "$lffish"
+#     end
 #
 
 function lf
