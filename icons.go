18d6e11b84021a5de39bd7c97f57246f85eddcd1
Mohsin Kaleem <mohkale@kisara.moe>
2020-11-22 15:16:33 +0000

    (icons): more flexible icons
    This patch gives lf the ability to support more icon patterns. Alongside the previous
    approach of matching a file extension (*.txt, *.png, etc.) you can now also specify:
    - complete file paths. eg. ~/downloads=foo
    - entire basenames. eg. CMakeLists.txt=bar
    - basenames excluding a file extension. eg. CMakeLists.*=baz
    - file extensions. eg. *.png=bag
    - file type classifications. eg. ex=bam
    
    NOTE: The current implementation tries each of these different possible paths in the
    same order shown here. That is exact paths have the highest precedence and then we try
    lower precedence possibles until eventually landing on either a generic file icon (fi=?)
    or a " ".

--- icons.go
+++ icons.go
@@ -45,46 +45,85 @@ func parseIconsEnv(env string) iconMap {
 	return icons
 }
 
+// Maps a file to a possible key in the iconMap.
+type iconKeyGenerator func(f *file) (string, bool)
+
+// Slice of transformation functions, each of which tries to convert
+// an input file to a key in iconMap. The first key that's found AND
+// exists in the iconMap is used to set the icon for the current
+// file.
+var iconKeyGenerators []iconKeyGenerator = []iconKeyGenerator{
+	// Firstly try to match the complete path (without tilde expansion).
+	func(f *file) (string, bool) {
+		path := f.path
+		home := gUser.HomeDir
+		if strings.HasPrefix(path, home) {
+			path = "~" + path[len(home):]
+		}
+		return path, true
+	},
+	// The exact basename of the file.
+	func(f *file) (string, bool) {
+		return filepath.Base(f.Name()), true
+	},
+	// The basename of the file excluding the extension
+	func(f *file) (string, bool) {
+		return filepath.Base(f.Name()) + ".*", true
+	},
+	// The file extension
+	func(f *file) (string, bool) {
+		return "*" + filepath.Ext(f.Name()), true
+	},
+	// The filetype classification
+	func(f *file) (string, bool) {
+		var key string
+
+		switch {
+		case f.IsDir() && f.Mode()&os.ModeSticky != 0 && f.Mode()&0002 != 0:
+			key = "tw"
+		case f.IsDir() && f.Mode()&os.ModeSticky != 0:
+			key = "st"
+		case f.IsDir() && f.Mode()&0002 != 0:
+			key = "ow"
+		case f.IsDir():
+			key = "di"
+		case f.linkState == working:
+			key = "ln"
+		case f.linkState == broken:
+			key = "or"
+		case f.Mode()&os.ModeNamedPipe != 0:
+			key = "pi"
+		case f.Mode()&os.ModeSocket != 0:
+			key = "so"
+		case f.Mode()&os.ModeCharDevice != 0:
+			key = "cd"
+		case f.Mode()&os.ModeDevice != 0:
+			key = "bd"
+		case f.Mode()&os.ModeSetuid != 0:
+			key = "su"
+		case f.Mode()&os.ModeSetgid != 0:
+			key = "sg"
+		case f.Mode().IsRegular() && f.Mode()&0111 != 0:
+			key = "ex"
+		default:
+			return "", false
+		}
+
+		return key, true
+	},
+	// If all else fails, everything is a file
+	func(f *file) (string, bool) {
+		return "fi", true
+	},
+}
+
 func (im iconMap) get(f *file) string {
-	var key string
-
-	switch {
-	case f.IsDir() && f.Mode()&os.ModeSticky != 0 && f.Mode()&0002 != 0:
-		key = "tw"
-	case f.IsDir() && f.Mode()&os.ModeSticky != 0:
-		key = "st"
-	case f.IsDir() && f.Mode()&0002 != 0:
-		key = "ow"
-	case f.IsDir():
-		key = "di"
-	case f.linkState == working:
-		key = "ln"
-	case f.linkState == broken:
-		key = "or"
-	case f.Mode()&os.ModeNamedPipe != 0:
-		key = "pi"
-	case f.Mode()&os.ModeSocket != 0:
-		key = "so"
-	case f.Mode()&os.ModeCharDevice != 0:
-		key = "cd"
-	case f.Mode()&os.ModeDevice != 0:
-		key = "bd"
-	case f.Mode()&os.ModeSetuid != 0:
-		key = "su"
-	case f.Mode()&os.ModeSetgid != 0:
-		key = "sg"
-	case f.Mode().IsRegular() && f.Mode()&0111 != 0:
-		key = "ex"
-	default:
-		key = "*" + filepath.Ext(f.Name())
-	}
-
-	if val, ok := im[key]; ok {
-		return val
-	}
-
-	if val, ok := im["fi"]; ok {
-		return val
+	for _, gen := range iconKeyGenerators {
+		if key, ok := gen(f); ok {
+			if val, ok := im[key]; ok {
+				return val
+			}
+		}
 	}
 
 	return " "
