8104dc17d54db337b81134410d8b19726f8f9846
sitiom <56180050+sitiom@users.noreply.github.com>
2020-12-22 18:06:17 +0800

    Add PowerShell keybinding example

--- etc/lfcd.ps1
+++ etc/lfcd.ps1
@@ -1,6 +1,15 @@
 # Change working dir in powershell to last dir in lf on exit.
 #
 # You need to put this file to a folder in $ENV:PATH variable.
+#
+# You may also like to assign a key to this command:
+# 
+# Set-PSReadLineKeyHandler -Chord Ctrl+o -ScriptBlock {
+#     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+#     [Microsoft.PowerShell.PSConsoleReadLine]::Insert('lfcd.ps1')
+#     [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+# }
+#
 
 $tmp = [System.IO.Path]::GetTempFileName()
 lf -last-dir-path="$tmp" $args
