930364ce7864d21fa70cd4214330886c953a42f1
Gokcehan <gokcehankara@gmail.com>
2022-01-22 16:57:28 +0300

    Revert "dirtyfiles: files that don't require cleaning before previewing them (#729)"
    This reverts commit 673dad9f070edd86494232232c620058d49ba5ef.

--- app.go
+++ app.go
@@ -481,7 +481,6 @@ func (app *app) runShell(s string, args []string, prefix string) {
 		cmd.Stderr = os.Stderr
 
 		app.nav.previewChan <- ""
-		app.nav.previewChan <- "!"
 		if err := app.ui.suspend(); err != nil {
 			log.Printf("suspend: %s", err)
 		}
