761aa2348379ea083d4cb510be57870cfc31409a
Christian Zangl <laktak@cdak.net>
2022-02-04 21:32:34 +0100

    add select-path to -help (#749)

--- main.go
+++ main.go
@@ -185,6 +185,18 @@ func checkServer() {
 }
 
 func main() {
+	flag.Usage = func() {
+		f := flag.CommandLine.Output()
+		fmt.Fprintln(f, "lf - Terminal file manager")
+		fmt.Fprintln(f, "")
+		fmt.Fprintf(f, "Usage:  %s [options] [select-path]\n\n", os.Args[0])
+		fmt.Fprintln(f, "  select-path")
+		fmt.Fprintln(f, "        set the initial file selection to the given argument")
+		fmt.Fprintln(f, "")
+		fmt.Fprintln(f, "Options:")
+		flag.PrintDefaults()
+	}
+
 	showDoc := flag.Bool(
 		"doc",
 		false,
