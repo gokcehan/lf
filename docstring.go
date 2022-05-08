a89f0242bf6d7442274cb0dc6f653ffe192c23d3
Gokcehan <gokcehankara@gmail.com>
2022-05-08 19:52:59 +0300

    update doc
    cc #826

--- docstring.go
+++ docstring.go
@@ -971,7 +971,7 @@ Characters from '#' to newline are comments and ignored:
 
     # comments start with '#'
 
-There are three special commands ('set', 'map', 'cmap', and 'cmd') for
+There are four special commands ('set', 'map', 'cmap', and 'cmd') for
 configuration.
 
 Command 'set' is used to set an option which can be boolean, integer, or
