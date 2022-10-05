7797aa93487c9587a992eee891640f970c0c32a0
Paul Ouellette <oue.paul18@gmail.com>
2022-10-04 22:29:34 -0500

    Revert escaping changes from cb7aa13
    The changes introduced a bug where escapes were removed when there are
    no completion matches.

--- complete.go
+++ complete.go
@@ -286,7 +286,6 @@ func matchExec(s string) (matches []string, longest []rune) {
 }
 
 func matchFile(s string) (matches []string, longest []rune) {
-	s = unescape(s)
 	dir := replaceTilde(s)
 
 	if !filepath.IsAbs(dir) {
@@ -298,7 +297,7 @@ func matchFile(s string) (matches []string, longest []rune) {
 		}
 	}
 
-	dir = filepath.Dir(dir)
+	dir = filepath.Dir(unescape(dir))
 
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
@@ -314,13 +313,13 @@ func matchFile(s string) (matches []string, longest []rune) {
 		}
 
 		_, last := filepath.Split(s)
-		if !strings.HasPrefix(f.Name(), last) {
+		if !strings.HasPrefix(escape(f.Name()), last) {
 			continue
 		}
 
 		name = f.Name()
 		if isRoot(s) || filepath.Base(s) != s {
-			name = filepath.Join(filepath.Dir(s), f.Name())
+			name = filepath.Join(filepath.Dir(unescape(s)), f.Name())
 		}
 		name = escape(name)
 
