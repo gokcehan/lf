f445aefd17211dee3efd500d9886a9ba3bd254e6
Joe Lim <50560759+joelim-work@users.noreply.github.com>
2023-05-12 18:48:56 +1000

    Alternate implementation

--- eval.go
+++ eval.go
@@ -1447,7 +1447,7 @@ func (e *callExpr) eval(app *app, args []string) {
 			return
 		}
 		var moved bool
-		if e.count == 0 {
+		if e.count == 1 {
 			moved = app.nav.top()
 		} else {
 			moved = app.nav.move(e.count - 1)
@@ -1461,7 +1461,7 @@ func (e *callExpr) eval(app *app, args []string) {
 			return
 		}
 		var moved bool
-		if e.count == 0 {
+		if e.count == 1 {
 			moved = app.nav.bottom()
 		} else {
 			moved = app.nav.move(e.count - 1)
