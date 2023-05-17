79e9e8698d98e771e8de9ad9d7e985cf5f20744b
Ching Pei Yang <badnam3o.0@gmail.com>
2023-05-17 13:20:52 +0200

    fixup! fix typo

--- eval.go
+++ eval.go
@@ -911,7 +911,7 @@ func (e *setExpr) eval(app *app, args []string) {
 			app.ui.echoerrf("sixel!: unexpected value: %s", e.val)
 			return
 		}
-		gOpts.wrapscroll = !gOpts.wrapscroll
+		gOpts.sixel = !gOpts.sixel
 	default:
 		// any key with the prefix user_ is accepted as a user defined option
 		if strings.HasPrefix(e.opt, "user_") {
