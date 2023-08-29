a2df1e6e1e619cb3b62739cbf09333f15c0e9b42
Joe Lim <50560759+joelim-work@users.noreply.github.com>
2023-08-29 16:09:43 +1000

    Update `lfcd.sh` example to use new flag

--- etc/lfcd.sh
+++ etc/lfcd.sh
@@ -15,16 +15,6 @@
 #
 
 lfcd () {
-    tmp="$(mktemp)"
     # `command` is needed in case `lfcd` is aliased to `lf`
-    command lf -last-dir-path="$tmp" "$@"
-    if [ -f "$tmp" ]; then
-        dir="$(cat "$tmp")"
-        rm -f "$tmp"
-        if [ -d "$dir" ]; then
-            if [ "$dir" != "$(pwd)" ]; then
-                cd "$dir"
-            fi
-        fi
-    fi
+    cd "$(command lf -print-last-dir "$@")"
 }
