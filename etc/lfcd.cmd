83919cbc0d97d2fe10979ae1e5af63732567ffaa
Abderrahmane TAHRI JOUTI <302837+atahrijouti@users.noreply.github.com>
2023-09-26 11:39:57 +0200

    exit successfully

--- etc/lfcd.cmd
+++ etc/lfcd.cmd
@@ -7,9 +7,9 @@ rem You need to put this file to a folder in %PATH% variable.
 set tmpfile="%tmp%\lf.%random%.tmp"
 if exist %tmpfile% goto:tmploop
 lf -last-dir-path=%tmpfile% %*
-if not exist %tmpfile% exit 1
+if not exist %tmpfile% exit 0
 set /p dir=<%tmpfile%
 del /f %tmpfile%
-if not exist "%dir%" exit 1
-if "%dir%" == "%cd%" exit 1
+if not exist "%dir%" exit 0
+if "%dir%" == "%cd%" exit 0
 cd /d "%dir%"
