46524b15e0d8a014fdacb44e73af7ea260f3f85d
Alexander Lunegov <alunegov@gmail.com>
2023-11-07 18:46:20 +0500

    Use `-print-last-dir` option for `lfcd.ps1`

--- etc/lfcd.ps1
+++ etc/lfcd.ps1
@@ -13,14 +13,4 @@
 # You may put this in one of the profiles found in $PROFILE.
 #
 
-$tmp = [System.IO.Path]::GetTempFileName()
-lf -last-dir-path="$tmp" $args
-if (Test-Path -PathType Leaf "$tmp") {
-    $dir = Get-Content "$tmp"
-    Remove-Item -Force "$tmp"
-    if (Test-Path -PathType Container "$dir") {
-        if ("$dir" -ne "$pwd") {
-            cd "$dir"
-        }
-    }
-}
+lf -print-last-dir $args | cd
