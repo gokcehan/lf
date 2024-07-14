ddd118f7344cefc9d6f0ba4ab50333ab41c272a4
BPplays <andymalbp@gmail.com>
2024-07-14 07:40:15 -0700

    changed to draw icon styled space after icon to fix windows terminal

--- ui.go
+++ ui.go
@@ -502,6 +502,7 @@ func (win *win) printDir(ui *ui, dir *dir, context *dirContext, dirStyle *dirSty
 
 		if icon.hasStyle && i != dir.pos {
 			win.print(ui.screen, lnwidth+2, i, icon.style, icon.icon)
+			win.print(ui.screen, lnwidth+3, i, icon.style, " ")
 		}
 
 		tag, ok := context.tags[path]
