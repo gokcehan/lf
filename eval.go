1c56620f571b5f40bac2a3418ba3f9a2df8ebd79
Oscar Wallberg <oscar.wallberg@pm.me>
2024-09-13 16:25:26 +0200

    feat: add mode, user and group info parameters (#1799)
    * feat: add mode, user and group info parameters
    
    * fix: correct function names
    
    * fix: change mode to perm

--- eval.go
+++ eval.go
@@ -260,9 +260,9 @@ func (e *setExpr) eval(app *app, args []string) {
 		toks := strings.Split(e.val, ":")
 		for _, s := range toks {
 			switch s {
-			case "size", "time", "atime", "ctime":
+			case "size", "time", "atime", "ctime", "perm", "user", "group":
 			default:
-				app.ui.echoerr("info: should consist of 'size', 'time', 'atime' or 'ctime' separated with colon")
+				app.ui.echoerr("info: should consist of 'size', 'time', 'atime', 'ctime', 'perm', 'user' or 'group' separated with colon")
 				return
 			}
 		}
@@ -482,9 +482,9 @@ func (e *setLocalExpr) eval(app *app, args []string) {
 		toks := strings.Split(e.val, ":")
 		for _, s := range toks {
 			switch s {
-			case "size", "time", "atime", "ctime":
+			case "size", "time", "atime", "ctime", "perm", "user", "group":
 			default:
-				app.ui.echoerr("info: should consist of 'size', 'time', 'atime' or 'ctime' separated with colon")
+				app.ui.echoerr("info: should consist of 'size', 'time', 'atime', 'ctime', 'perm', 'user' or 'group' separated with colon")
 				return
 			}
 		}
