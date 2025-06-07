3f3466613571f553ec9699684e88a6117f50015f
Joe Lim <50560759+joelim-work@users.noreply.github.com>
2025-06-07 17:59:09 +1000

    Revert "Reload if any component in current path is renamed (#1982)"
    This reverts commit 12f235fac9cde43b46e500d622c2ef51536d52d4.

--- app.go
+++ app.go
@@ -404,18 +404,13 @@ func (app *app) loop() {
 				oldCurrPath = curr.path
 			}
 
-			if wd, err := os.Getwd(); err == nil && wd != app.nav.currDir().path {
-				// if any path component in the current path is renamed, then
-				// the entire set of directories has to be reloaded
-				app.nav.cd(wd)
-			} else {
-				for i := range app.nav.dirs {
-					if app.nav.dirs[i].path == d.path {
-						app.nav.dirs[i] = d
-					}
+			for i := range app.nav.dirs {
+				if app.nav.dirs[i].path == d.path {
+					app.nav.dirs[i] = d
 				}
-				app.nav.position()
 			}
+
+			app.nav.position()
 
 			curr, err := app.nav.currFile()
 			if err == nil {
