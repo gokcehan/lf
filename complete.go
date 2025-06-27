e9275b0f3b6bd680fb93c1df176f8194b3e4d4b4
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2025-06-27 23:28:07 +0200

    Add missing completions

--- complete.go
+++ complete.go
@@ -14,6 +14,7 @@ var (
 		"set",
 		"setlocal",
 		"map",
+		"vmap",
 		"cmap",
 		"cmd",
 		"quit",
@@ -110,6 +111,7 @@ var (
 		"cmd-uppercase-word",
 		"cmd-lowercase-word",
 		"visual",
+		"visual-change",
 		"visual-accept",
 		"visual-discard",
 	}
@@ -331,7 +333,7 @@ func completeCmd(acc []rune) (matches []string, longestAcc []rune) {
 		case "set":
 			matches, longest = matchWord(f[1], gOptWords)
 			longestAcc = append(acc[:len(acc)-len([]rune(f[len(f)-1]))], longest...)
-		case "map", "cmap", "cmd":
+		case "map", "vmap", "cmap", "cmd":
 			longestAcc = acc
 		default:
 			matches, longest = matchFile(f[len(f)-1])
@@ -342,7 +344,7 @@ func completeCmd(acc []rune) (matches []string, longestAcc []rune) {
 		case "setlocal":
 			matches, longest = matchWord(f[2], gLocalOptWords)
 			longestAcc = append(acc[:len(acc)-len([]rune(f[len(f)-1]))], longest...)
-		case "map", "cmap":
+		case "map", "vmap", "cmap":
 			matches, longest = matchCmd(f[2])
 			longestAcc = append(acc[:len(acc)-len([]rune(f[len(f)-1]))], longest...)
 		case "addcustominfo":
@@ -353,7 +355,7 @@ func completeCmd(acc []rune) (matches []string, longestAcc []rune) {
 		}
 	default:
 		switch f[0] {
-		case "set", "setlocal", "map", "cmap", "cmd":
+		case "set", "setlocal", "map", "vmap", "cmap", "cmd":
 			longestAcc = acc
 		default:
 			matches, longest = matchFile(f[len(f)-1])
