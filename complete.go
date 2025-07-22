bc48aebf3bfe2bee36733b9c3f9c9fba1ac633c1
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2025-07-22 20:25:28 +0200

    Add completions for `filtermethod` and `searchmethod`

--- complete.go
+++ complete.go
@@ -343,6 +343,8 @@ func completeCmd(acc []rune) (matches []string, longestAcc []rune) {
 			break
 		}
 		switch f[1] {
+		case "filtermethod", "searchmethod":
+			matches, longest = matchWord(f[2], []string{"text", "glob", "regex"})
 		case "selmode":
 			matches, longest = matchWord(f[2], []string{"all", "dir"})
 		case "sortby":
