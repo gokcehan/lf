793aeca0f8fd419fd78d3d705634a244d46f560e
Joe Lim <50560759+joelim-work@users.noreply.github.com>
2025-09-20 17:56:23 +1000

    reword2

--- misc.go
+++ misc.go
@@ -294,8 +294,8 @@ func humanize(size uint64) string {
 // This function compares two strings for natural sorting which takes into
 // account the values of numbers in strings. For example, '2' is ordered before
 // '10', and similarly 'foo2bar' ordered before 'foo10bar'. When comparing
-// numbers, if they have the same value then the length of the string is
-// also compared, so '0' is ordered before '00'.
+// numbers, if they have the same value then the length of the string is also
+// compared, so '0' is ordered before '00'.
 func naturalCmp(s1, s2 string) int {
 	s1len := len(s1)
 	s2len := len(s2)
