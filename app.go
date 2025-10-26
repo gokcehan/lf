619b6b0cd89d3ab80f95a948cf200ca374ff5ad6
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2025-10-26 21:42:38 +0100

    Explicitly hide `mode` in `showbinds` menu

--- app.go
+++ app.go
@@ -562,16 +562,16 @@ func (app *app) runShell(s string, args []string, prefix string) {
 	gState.data["maps"] = listBinds(map[string]map[string]expr{
 		"n": gOpts.nkeys,
 		"v": gOpts.vkeys,
-	})
+	}, true)
 	gState.data["nmaps"] = listBinds(map[string]map[string]expr{
 		"n": gOpts.nkeys,
-	})
+	}, true)
 	gState.data["vmaps"] = listBinds(map[string]map[string]expr{
 		"v": gOpts.vkeys,
-	})
+	}, true)
 	gState.data["cmaps"] = listBinds(map[string]map[string]expr{
 		"c": gOpts.cmdkeys,
-	})
+	}, true)
 	gState.data["cmds"] = listCmds(gOpts.cmds)
 	gState.data["jumps"] = listJumps(app.nav.jumpList, app.nav.jumpListInd)
 	gState.data["history"] = listHistory(app.cmdHistory)
