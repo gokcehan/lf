ca97b136eff7440ddc06ced6e75b78b0c43a5356
CatsDeservePets <145048791+CatsDeservePets@users.noreply.github.com>
2026-03-20 11:09:04 +0100

    Revert "refactor: remove `drawbox` option"
    This reverts commit 9a00a5a6acca07af73decb7dfb2659f602238d1a.

--- opts.go
+++ opts.go
@@ -74,6 +74,7 @@ var gOpts struct {
 	dironly          bool
 	dirpreviews      bool
 	drawborders      border
+	drawbox          bool
 	dupfilefmt       string
 	errorfmt         string
 	filesep          string
@@ -217,6 +218,7 @@ func init() {
 	gOpts.dironly = false
 	gOpts.dirpreviews = false
 	gOpts.drawborders = none
+	gOpts.drawbox = false
 	gOpts.dupfilefmt = "%f.~%n~"
 	gOpts.errorfmt = "\033[7;31;47m"
 	gOpts.filesep = "\n"
